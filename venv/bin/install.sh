914f5abfbaa0762c69674e7bc52b37867b12ad02
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-02-22 14:21:06 +1100

    Install pip with easy_install

--- venv/bin/install.sh
+++ venv/bin/install.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cp /usr/bin/python $DIR
-cp /usr/bin/pip $DIR
+
+$DIR/easy_install pip
 
 # Activate the venv by default
 su root -c "echo 'source $DIR/activate' >> /etc/profile"
