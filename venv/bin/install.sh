ab4dee68007eaa4c202120e2ae4f117636765888
Aitor Miguel Blanco <aitormibl@gmail.com>
2021-02-22 13:48:33 +1100

    Finished installation script
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- venv/bin/install.sh
+++ venv/bin/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp /usr/bin/python .
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cp /usr/bin/python $DIR
+cp /usr/bin/pip $DIR
 
 # Activate the venv by default
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-su root -c "echo 'source $DIR/activate' >> /etc/bash/bashrc"
+su root -c "echo 'source $DIR/activate' >> /etc/profile"
